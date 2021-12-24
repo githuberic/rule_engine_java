17ea0888f2206eb693faa4b030135af4be009c7e
lgq <liugq@weipaitang.com>
2021-12-25 01:13:52 +0800

    project init

---
+++ src/main/java/com/lgq/re/RuleEngineJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lgq.re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEngineJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgq/re/RuleEngineJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgq.re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
